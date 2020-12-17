a3c3e915801aed417a09ba898994b179d3562714
Cyber-Sammy <Cyber-Sammy@users.noreply.github.com>
2020-12-17 20:59:16 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PL;
+
+namespace Labun_Lab_3_3_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.MainMenu();
+        }
+    }
+}
